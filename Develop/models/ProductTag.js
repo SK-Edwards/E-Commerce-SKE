6e4a7b96ee38baf3e7b7e25f2a4b6fa78b6847aa
SK. Edwards <125165568+SK-Edwards@users.noreply.github.com>
2023-05-12 13:26:26 -0400

    Finished Models

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -1,11 +1,25 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
+const internal = require('stream');
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id:{
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Product',
+        key: 'id',
+      }
+    },
     // define columns
   },
   {
