bbe56da00c6151dfe697d953fd20afe6d16cf226
SK. Edwards <125165568+SK-Edwards@users.noreply.github.com>
2023-05-12 12:52:12 -0400

    Built product model

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,6 +9,41 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
+    id: {
+    type: DataTypes.INTEGER,
+    allowsNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  product_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.DECIMAL,
+    allowNull: false,
+    validate:{
+      isDecimal: true,
+    }
+  },
+  stock: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    validate: {
+      isNumeric: true,
+      min: 10,
+    }
+  },
+  category_id:{
+    type: DataTypes.INTEGER,
+    foreignKey: true,
+    references: {
+      model: 'Category',
+      key: 'id',
+
+    }
+
+  }
     // define columns
   },
   {
