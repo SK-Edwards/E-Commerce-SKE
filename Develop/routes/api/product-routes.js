04d80de065ebf0327ba84f6f797b5984c67a1233
SK. Edwards <125165568+SK-Edwards@users.noreply.github.com>
2023-05-12 22:53:08 -0400

    Category Routes Completed

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -6,7 +6,18 @@ const { Product, Category, Tag, ProductTag } = require('../../models/Product');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  Product.findAll().then((productData) => {
+  Product.findAll({
+    include: [
+      {
+        model: Category,
+        attributes: ['category_name'],
+      },
+      {
+        model: Tag,
+        attribute:['tag_name'] ,
+      },
+    ],
+  }).then((productData) => {
     res.json(productData);
     console.log(productData)
   });
@@ -14,12 +25,29 @@ router.get('/', (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
- Product.findAll().then((productData) => {
-    res.json(productData);
-    console.log(productData)
-  });
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [
+        {
+          model: Category,
+          attributes: ['category_name'],
+        },
+        {
+          model: Tag,
+          attribute:['tag_name'] ,
+        },
+      ],
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No user with this id!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 
   // be sure to include its associated Category and Tag data
 });
